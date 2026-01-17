d4adf4cb18ca2ee5af8be4cd5be978645c31d023
Paulus <pwilhem@mtc.com.na>
2026-01-17 13:27:47 +0200

    appTokenMiddleware, checkAppUser

--- routes/app/specializationRoute.js
+++ routes/app/specializationRoute.js
@@ -1,9 +1,9 @@
 const { Router } = require('express');
 const { getAllSpecializations } = require('../../controllers/app/specializationController');
-const { tokenAuthMiddleware, checkUser } = require('../../middlewares/authMiddleware');
+const { tokenAuthMiddleware, checkUser, appTokenMiddleware, checkAppUser } = require('../../middlewares/authMiddleware');
 
 const specializationRouter = Router();
 
-specializationRouter.get('/all-specializations',tokenAuthMiddleware,checkUser, getAllSpecializations);
+specializationRouter.get('/all-specializations',appTokenMiddleware, checkAppUser, getAllSpecializations);
 
 module.exports = specializationRouter;
